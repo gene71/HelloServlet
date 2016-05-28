ea8207e9c2454e7a1a047820b8aee594222db54f
gene <gene@192.168.176.130>
2016-05-27 19:58:08 -0700

    new class added

--- HelloClient/src/com/example/client/Main.java
+++ HelloClient/src/com/example/client/Main.java
@@ -1,10 +1,50 @@
 package com.example.client;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.URL;
+import java.net.URLConnection;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import com.google.gson.Gson;
+
+public class Main {
+	
+
+	public static void main(String[] args) throws Exception{
+		System.out.println("Post Test");
+        User u = new User();
+        u.setfName("Gene");
+        u.setlName("Moore");
+        u.setTitle("SWE");
+        //JSONBuilder jb = new JSONBuilder();
+
+
+         URL url = new URL("http://localhost:8080/HelloTomcat/Hello");
+         URLConnection conn = url.openConnection();
+         conn.setDoOutput(true);
+         OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+         
+         //(Dependency) Use the google gson parser located here: https://github.com/google/gson
+         Gson gson = new Gson();
+         String json = gson.toJson(u);  
+
+         writer.write(json);
+         writer.flush();
+         System.out.println("posted to server: " + json);
+
+         //read the post back
+         String line;
+         BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream()));
+            while ((line = reader.readLine()) != null) {
+              System.out.println("recieved from server: " + line);
+            }
+         writer.close();
+         reader.close();
 
 	}
+	
 
 }
+
+
---
+++ HelloClient/src/com/example/client/User.java
@@ -0,0 +1,26 @@
+package com.example.client;
+
+//simple container class for testing
+public class User{
+			String fName;
+			String lName;
+			String title;
+			public String getfName() {
+				return fName;
+			}
+			public void setfName(String fName) {
+				this.fName = fName;
+			}
+			public String getlName() {
+				return lName;
+			}
+			public void setlName(String lName) {
+				this.lName = lName;
+			}
+			public String getTitle() {
+				return title;
+			}
+			public void setTitle(String title) {
+				this.title = title;
+			}
+}
